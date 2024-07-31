066860c0368d17c011e1790cb1d088d7f355602a
Satyam Choudhary <Satyamchoudhary0007@gmail.com>
2024-07-31 14:08:50 +0530

    Minor Changes

--- index.js
+++ index.js
@@ -10,26 +10,25 @@ const PORT_NUMBER = process.env.PORT_NUMBER || 4300;
 DBConn(process.env.DB_URL);
 
 // //Importing Routes
- const userRoute = require("./routers/userRoute");
- const expenseRoute = require("./routers/expenseRoute");
- const purchaseRoute = require("./routers/purchaseRoute");
+const userRoute = require("./routers/userRoute");
+const expenseRoute = require("./routers/expenseRoute");
+const purchaseRoute = require("./routers/purchaseRoute");
 const premiumRoute = require("./routers/premiumRoute");
 const passwordRoute = require("./routers/passwordRoute");
 
 //Application Level Middlewares
 app.use(bodyParser.urlencoded({ extended: false }));
 app.use(bodyParser.json());
-app.use(express.static("public"));
+app.use(express.static(__dirname + "/public"));
 
 //Route Level Middlewares
 app.use("/", userRoute);
 app.use("/user", userRoute);
 app.use("/expense", expenseRoute);
- app.use("/purchase", purchaseRoute);
+app.use("/purchase", purchaseRoute);
 app.use("/premium", premiumRoute);
 app.use("/password", passwordRoute);
 
-
-app.listen(5800,()=>{
-    console.log("Server Started")
-})
+app.listen(5800, () => {
+  console.log("Server Started");
+});
