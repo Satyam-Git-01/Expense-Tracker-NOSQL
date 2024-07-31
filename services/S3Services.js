35342147647ab3f5ed76993be9e17b79a9544d18
Satyam Choudhary <Satyamchoudhary0007@gmail.com>
2024-07-31 13:10:10 +0530

    Variables Change

--- services/S3Services.js
+++ services/S3Services.js
@@ -1,12 +1,12 @@
 const AWS= require('aws-sdk')
 const uploadTos3 = async (data, fileName) => {
-  const BUCKET_NAME = process.env.AWS_BUCKET_NAME;
-  const AWS_ACCESS_KEY = process.env.AWS_ACCESS_KEY;
-  const AWS_SECRET_KEY = process.env.AWS_SECRET_KEY;
+  const BUCKET_NAME = process.env.MY_AWS_BUCKET_NAME;
+  const MY_AWS_ACCESS_KEY = process.env.MY_AWS_ACCESS_KEY;
+  const MY_AWS_SECRET_KEY = process.env.MY_AWS_SECRET_KEY;
 
   let s3bucket = new AWS.S3({
-    accessKeyId: AWS_ACCESS_KEY,
-    secretAccessKey: AWS_SECRET_KEY,
+    accessKeyId: MY_AWS_ACCESS_KEY,
+    secretAccessKey: MY_AWS_SECRET_KEY,
   });
 
   let params = {
